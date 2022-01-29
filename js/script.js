30a87ce1d36c4982a43a6635f973e595bfc4a82f
wewjr82 <68568420+wewjr82@users.noreply.github.com>
2022-01-28 23:42:21 -0500

    touching up mobile

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ let booked = document.querySelector("#booking");
 
 let view = document.querySelector("#view_more");
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector('ul');
+
 follow.addEventListener("click", function () {
   document.querySelector("#follow_twitter").innerText = "WE'LL FOLLOW BACK";
 });
@@ -15,3 +18,19 @@ booked.addEventListener("click", function () {
 view.addEventListener("click", function () {
   document.querySelector("#view_button").innerText = "AWESOME!!!";
 });
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("a");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
